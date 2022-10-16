1d7a01d97e0b32be515c108e7a6af597689e55e7
EmreDegirmenci <emre.degirmenci@textalkmedia.se>
2022-10-16 15:33:01 +0300

    Adds BreedImages model object

---
+++ DogSearch/DTOs/BreedImages.swift
@@ -0,0 +1,15 @@
+//
+//  BreedImages.swift
+//  DogSearch
+//
+//  Created by TextalkMedia-Emre on 2022-10-12.
+//
+
+import Foundation
+
+
+// MARK: - BreedImages
+struct BreedImages: Codable, Hashable, Equatable {
+  var message: [String]
+    let status: String
+}
