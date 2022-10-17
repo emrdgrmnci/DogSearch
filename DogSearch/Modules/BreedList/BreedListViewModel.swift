58476aeaf4fe4232db5a1023ac209b827f17740d
EmreDegirmenci <emre.degirmenci@textalkmedia.se>
2022-10-17 13:12:28 +0300

    Changes variable name

--- DogSearch/Modules/BreedList/BreedListViewModel.swift
+++ DogSearch/Modules/BreedList/BreedListViewModel.swift
@@ -10,31 +10,33 @@ import UIKit
 import Combine
 
 final class BreedListViewModel {
-
+  
   //MARK: - Variables
   weak var delegate: BreedListViewModelDelegate?
-  private let service: BreedListClientProtocol
+  private let breedListClient: BreedListClientProtocol
+  private let breedListDetailClient: BreedListDetailClientProtocol
   private var breeds: DogBreed
   private var breedImages: BreedImages
-
+  
   var subscriptions: Set<AnyCancellable> = []
-
+  
   //Dependency Injection
-  init(service: BreedListClientProtocol) {
-    self.service = service
+  init(breedListClient: BreedListClientProtocol, breedListDetailClient: BreedListDetailClientProtocol) {
+    self.breedListClient = breedListClient
+    self.breedListDetailClient = breedListDetailClient
     self.breeds = DogBreed(message: [String : [String]](), status: String())
     self.breedImages = BreedImages(message: [String](), status: String())
   }
 }
 
 extension BreedListViewModel: BreedListViewModelProtocol {
-
+  
   //MARK: - Get Breed List from API
   func load() {
     notify(.setTitle("Dog Breeds"))
     notify(.setLoading(true))
-
-    service.getBreeds()
+    
+    breedListClient.getBreeds()
       .sink(receiveCompletion: { (completion) in
         switch completion {
         case .finished:
@@ -50,11 +52,11 @@ extension BreedListViewModel: BreedListViewModelProtocol {
       }
       .store(in: &subscriptions)
   }
-
+  
   //MARK: - Select Breead at Index
   func selectBreed(at index: Int, breedQuery: String) {
     if breeds.message.values.count > 0 {
-      service.getBreedImages(breedQuery: breedQuery)
+      breedListDetailClient.getBreedImages(breedQuery: breedQuery)
         .sink(receiveCompletion: { (completion) in
           switch completion {
           case .finished:
@@ -67,14 +69,14 @@ extension BreedListViewModel: BreedListViewModelProtocol {
           self?.notify(.setLoading(false))
           self?.delegate?.handleViewModelOutput(.showBreedImages(breedImages))
           self?.breedImages = breedImages
-
+          
           let viewModel = BreedListDetailViewModel(breedImageDetail: breedImages)
           self?.delegate?.navigate(to: .detail(viewModel))
         }
         .store(in: &subscriptions)
     }
   }
-
+  
   //MARK: - Notify Breed List View 
   private func notify(_ output: BreedListViewModelOutput) {
     delegate?.handleViewModelOutput(output)
